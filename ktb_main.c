81d24898fd0343bb1714b7dcd6645abd6bcab5c1
abysamross <abysamross@iitb.ac.in>
2016-07-20 14:57:28 +0530

    enabling pcd_disassociate() msgs.

--- ktb_main.c
+++ ktb_main.c
@@ -2426,10 +2426,10 @@ sysfssucc:
 	   debug(custom_radix_tree_destroy);
 	   debug(custom_radix_tree_node_destroy);
 	   debug(pcd_add_to_remote_tree);
-	   debug(pcd_disassociate);
 	   debug(pcd_remote_associate);
 	   debug(timed_fwd_filter);
 	   */
+	debug(pcd_disassociate);
 	debug(tmem_pgp_free_data);
 	debug(ktb_remotify_puts);
 	debug(ktb_remotified_get_page);
@@ -2468,6 +2468,7 @@ sysfssucc:
 	show_msg(ktb_remotified_get_page);
 	show_msg(ktb_remote_get);
 	show_msg(tmem_pgp_free_data);
+	show_msg(pcd_disassociate);
 	//end en/dis-able ktb_main.c output
 
 	//-------------------------
@@ -2477,7 +2478,6 @@ sysfssucc:
 	/*
 	   show_msg(pcd_remote_associate);
 	   show_msg(timed_fwd_filter);
-	   show_msg(pcd_disassociate);
 	   show_msg(pcd_add_to_remote_tree);
 	   show_msg(tmem_pgp_free);
 	   show_msg(tmem_pgp_destroy);
